d2ff0715905546151bcb8e3e1eba8a273183ef71
andreiday <sh4dow_day@yahoo.com>
2021-06-16 16:00:20 +0300

    xArm lib serial test

--- tasks/human_pose/test_serial.py
+++ tasks/human_pose/test_serial.py
@@ -1,7 +1,21 @@
-from serial_process import ServoComms
+import xarm
 
-def main():     
-    comms = ServoComms()
+arm = xarm.Controller('/dev/ttyTHS1', True)
 
-if __name__ == "__main__":
-    main()
+servo1 = xarm.Servo(1)
+servo2 = xarm.Servo(2)
+servo3 = xarm.Servo(3)
+servo4 = xarm.Servo(4)
+servo5 = xarm.Servo(5)
+servo6 = xarm.Servo(6)
+
+for i in range(0,6):
+    arm.setPosition(i, 1500)
+    print(i)
+# battery_voltage = arm.getBatteryVoltage()
+# pos1 = arm.getPosition(6)
+
+# print('Battery voltage (volts):', battery_voltage)
+# print('pos1:', pos1)
+
+# arm.setPosition([servo1, servo2, servo3, servo4, servo5, servo6]) 
