c9e6d15470ea103d85798c4c18d68daf170ef0ee
andreiday <sh4dow_day@yahoo.com>
2021-06-19 15:02:54 +0300

    keypoint follower

--- tasks/human_pose/tracking/follow_keypoint.py
+++ tasks/human_pose/tracking/follow_keypoint.py
@@ -1,45 +1,55 @@
-import PID
+import cv2
+import tracking.PID
 import xarm
 
-
-
-class face_follow:
+class KeypointFollow:
     def __init__(self):
-        self.Arm = xarm.Controller('/dev/ttyTHS1', True)
-
+        self.Arm = xarm.Controller('/dev/ttyTHS1', False)
         self.target_servox=90
         self.target_servoy=45
-        self.xservo_pid = PID.PositionalPID(0.5, 0.2, 0.31)
-        self.yservo_pid = PID.PositionalPID(0.5, 0.2, 0.35)
+        self.xservo_pid = tracking.PID.PositionalPID(5, 0.2, 0.35)
+        self.yservo_pid = tracking.PID.PositionalPID(7, 0.4, 0.5)
 
     def follow_function(self, x, y):
 
 
         point_x = x
         point_y = y
-        
+
         if not (self.target_servox>=180 and point_x<=320 or self.target_servox<=0 and point_x>=320):
             self.xservo_pid.SystemOutput = point_x
             self.xservo_pid.SetStepSignal(320)
-            self.xservo_pid.SetInertiaTime(0.01, 0.1)
-            target_valuex = int(1500 + self.xservo_pid.SystemOutput)
-            self.target_servox = int((target_valuex - 500) / 10)
 
-            if self.target_servox > 180:self.target_servox = 180
+            self.xservo_pid.SetInertiaTime(0.005, 0.01)
+            print("PID system output: ", self.xservo_pid.SystemOutput)
+
+            target_valuex = int(1500 + self.xservo_pid.SystemOutput)
+            print("target_valuex: ", target_valuex)
+            self.target_servox = int((target_valuex - 500) / 10)
+            print("target_servox: ", self.target_servox)
+
+            if self.target_servox > 180: self.target_servox = 180
             if self.target_servox < 0: self.target_servox = 0
-        if not (self.target_servoy>=180 and point_y<=240 or self.target_servoy<=0 and point_y>=240):
+        if not (self.target_servoy>=90 and point_y<=240 or self.target_servoy<=0 and point_y>=240):
 
             self.yservo_pid.SystemOutput = point_y
             self.yservo_pid.SetStepSignal(240)
-            self.yservo_pid.SetInertiaTime(0.01, 0.1)
-            target_valuey = int(1500 + self.yservo_pid.SystemOutput)
-            self.target_servoy = int((target_valuey - 500) / 10) - 45
 
-            if self.target_servoy > 360: self.target_servoy = 360
+
+            self.yservo_pid.SetInertiaTime(0.005, 0.01)
+            print("PID system output: ", self.yservo_pid.SystemOutput)
+
+            target_valuey = int(1500 + self.yservo_pid.SystemOutput)
+            print("target_valuey: ", target_valuey)
+
+            self.target_servoy = int((target_valuey - 500) / 10) - 20
+            print("target_servoy: ", self.target_servoy)
+
+            if self.target_servoy > 90: self.target_servoy = 90
             if self.target_servoy < 0: self.target_servoy = 0
-        joints_0 = [self.target_servox, 135, self.target_servoy / 2, self.target_servoy / 2, 90, 30]
-        
+
+        joints_0 = [90, 90, self.target_servoy / 2, self.target_servoy / 2, 55, self.target_servox]
         for i in range(len(joints_0)):
-            print(i, float(joints_0[i]))
-            # self.Arm.setPosition(i, float(joints_0[i]))
+            print("Servo id {}, angle: {}".format(i+1, float(joints_0[i])))
+            self.Arm.setPosition(i+1, float(joints_0[i]), 800)
         return
